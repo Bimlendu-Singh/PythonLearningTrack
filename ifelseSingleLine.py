b2b26201e65f29f6dc28691b3df39dbebb35733a
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-11-05 13:36:51 +0530

    Program added

---
+++ ifelseSingleLine.py
@@ -0,0 +1,3 @@
+a = int(input("Enter number: "))
+
+print("Even") if a%2==0 else print("Odd")
---
+++ ifleseGradeprogram.py
@@ -0,0 +1,14 @@
+marks=int(input("Enter Marks : "))
+
+if marks>90:
+    print("Grade A since marks is: ",marks)
+elif (marks>80):
+    print("Grade B since marks is: ",marks)
+elif marks>70:
+    print("Grade C since marks is: ",marks)
+elif marks>60:
+    print("Grade D since marks is: ",marks)
+elif marks>=50:
+    print("Grade E since marks is: ",marks)
+else:
+    print("Grade F since marks is: ",marks)
