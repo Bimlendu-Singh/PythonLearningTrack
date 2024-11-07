def9f0d297e763ce1cddd4297e05bfd5bad4c76d
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-11-07 13:23:35 +0530

    three digit number check program

---
+++ ThreeDigitNumberCheck.py
@@ -0,0 +1,8 @@
+x=int(input("Enter 3 digit number: "))
+if x<1000 and x>99:
+    print(x," is 3 digit number")
+else:
+    print(x," is not a 3 digit number")
+y=int(input("Enter 2 digit number: "))
+if 9 < y < 100:
+    print(y," is not a 2 digit number")
