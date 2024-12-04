0db3cb37839187904693afe524470851cf273d03
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-12-04 12:58:49 +0530

    sumof first n natural number qn

---
+++ Loops question/squaresOfFirstNaturalNumber.py
@@ -0,0 +1,5 @@
+i=1
+n=int(input("Enput n : "))
+while i<=n:
+    print("Square of ",i," is : ",(i*i))
+    i+=1
---
+++ sumOfFirstNNaturalNumber.py
@@ -0,0 +1,8 @@
+n=int(input("Enter n : "))
+i,sum = 1,0
+
+while i<=n:
+    sum+=i
+    i+=1
+
+print("Sum of first ",n," natural no. is: ",sum)
